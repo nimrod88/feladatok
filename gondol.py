430916c54385c284eeba463ba9325e6c04134281
nimrod88 <93653962+nimrod88@users.noreply.github.com>
2023-02-13 09:36:59 +0100

    Add files via upload

---
+++ gondol.py
@@ -0,0 +1,8 @@
+import random
+
+szam1=random.randint(1,5)
+szam2=int(input('Kérem adjon meg egy számot'))
+if szam1 == szam2:
+    print('A két szám megegyezik.')
+else:
+    print('A két szám különböző.')
