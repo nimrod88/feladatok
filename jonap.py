f99cb54970de5f847ff6ce2c7fb5624def125b0a
nimrod88 <93653962+nimrod88@users.noreply.github.com>
2023-02-13 09:32:35 +0100

    Add files via upload

---
+++ jonap.py
@@ -0,0 +1,7 @@
+nap = input('Jó napod van(i/n)?')
+if nap == 'i':
+    print('Akkor jó.')
+elif nap == 'n':
+    print('Akkor remélem jobb lesz.')
+else:
+    print('A válaszod kérem i vagy n legyen')
---
+++ paros.py
@@ -0,0 +1,5 @@
+szam = int(input('Kérem adjon meg egy számot! '))
+if szam % 2 == 0:
+    print('A szám páros.')
+else:
+    print('A szám páratlan.')
